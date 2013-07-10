218e3c34aa2c8e7a144f5d8c05c5687959497bba
Rafael <xample@stamford.edu>
2013-07-10 16:03:43 +0000

    web.js again 2

--- web.js
+++ web.js
@@ -4,13 +4,11 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
-fs = require('fs')
-fs.readFile('index.html', 'utf8', function(err,data) {
-  if(err){
-   response.send(err);
-}
-  response.write(data);
-  response.end();
+	fs = require('fs')
+	var buf = new Buffer (fs.readFileSync('index.html'), 'utf-8');
+	response.send(buf.toString());
+
+
 });
 
 var port = process.env.PORT || 5000;
