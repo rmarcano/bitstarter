5ad577839bd32dab69bbee1bd184609595596644
Rafael <xample@stamford.edu>
2013-07-10 15:35:08 +0000

    Final web.js

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+fs = require('fs')
+fs.readFile('index.html', 'utf8', function(err,data) {
+  if(err){
+   response.send(err);
+}
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
